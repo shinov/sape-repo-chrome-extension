8b6b49e537a6ffc4e5f55544844b7989cfb972b9
shinov <shinov4u@gmail.com>
2014-07-31 11:06:42 +0530

    Update anonymous.js

--- anonymous.js
+++ anonymous.js
@@ -1,1 +1,47 @@
-(function($, window, document, undefined) {    'use strict';    /* ----------------------------------------------------------------------------- *\     Private Properties     \* ----------------------------------------------------------------------------- */    var anonymous , defaults = {        sourceURL:"jqueygihub.json",        caregory :['Productivity tool','Web Application Frameworks']    };    function Anonymous() {        this.init();    };    Anonymous.prototype.init =  function() {        var thisInit =  this;        thisInit.repoFetch(defaults.sourceURL);        thisInit.initAlarm();    }    Anonymous.prototype.repoFetch =  function(getUrl) {        var thisInit =  this;        var getJSsonRepo = this.ajaxObj(getUrl);        getJSsonRepo.done(function(){            thisInit.loadTemplateCategory(getJSsonRepo.responseJSON , "main-category");            chrome.storage.local.set({'value': getJSsonRepo.responseJSON} , function() {                // Notify that we saved.                console.log('Settings saved');            });            chrome.storage.local.get('value', function(obj , items) {                // Notify that we saved.                console.log(obj);                console.log(items);                console.log('Settings get');            });            chrome.storage.onChanged.addListener(function(changes, namespace) {                console.log("data updated")            });        });    }    Anonymous.prototype.ajaxObj =  function(getUrl) {        return $.ajax({            url:getUrl,            dataType:'json',            async: true        });    };    Anonymous.prototype.initAlarm =  function() {        var thisinitAlarm = this;        chrome.alarms.create('periodicSucker', {            delayInMinutes : .3,            periodInMinutes: .3        });        chrome.alarms.onAlarm.addListener(function (alarm) {            if (alarm.name == 'periodicSucker') {                thisinitAlarm.repoFetch(defaults.sourceURL);            }        });    };    Anonymous.prototype.loadTemplateCategory =  function(getObjData , element) {        var template = "";        console.log(getObjData);        $("#main-category").html("");        for(var i=0 ; i < getObjData.length;i++){            template = template + "<div>"+getObjData[i]['description']+"</div>";        }        $("#main-category").html(template);    };    document.addEventListener('DOMContentLoaded', function () {        new Anonymous();    });})(jQuery, window, document,undefined);
+;(function ( Anonymous , chrome , $ ) {
+    
+	
+	var publicVars = {
+        githubAPIURL:"jqueygihub.json",
+        category :['Productivity tool','Web Application Frameworks'] // decide require or not
+    };
+	
+	
+	Anonymous.loadTemplateAPI = (function(){
+	  return {
+	    createCategory: function(getJsonData){
+		    getJsonData = getJsonData.anonymous;
+		    console.log(getJsonData);
+		
+			var template = "";
+			$("#main-category").html("");
+			for(var i=0 ; i < getJsonData.length;i++){
+			template = template + "<div>"+getJsonData[i]['description']+"</div>";
+			}
+			$("#main-category").html(template);
+		}
+	  }
+	
+	})();
+	
+	
+	
+    document.addEventListener('DOMContentLoaded', function () {
+        var localData = Anonymous.StorageAPI.getStorage();
+		localData.done(function( data ) {
+			if(typeof data.anonymous == "undefined"){
+			   console.log("nno data");
+			   Anonymous.RepoSuckerAPI.fetchRepo(publicVars.githubAPIURL);
+			}
+			else{
+			   console.log("with data");
+			   Anonymous.loadTemplateAPI.createCategory(data);
+			}
+			Anonymous.NotificationAPI.updateTrayIcon("icon.png");
+			chrome.browserAction.setBadgeText({text: ""});
+		});
+    });
+
+})(window.Anonymous = window.Anonymous || {} , chrome , jQuery);
+
+
